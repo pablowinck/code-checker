3ba3789d828d48d04e55ad8be198cf0b6a85ed05
Pablo Winter <pablowinck123@gmail.com>
2023-07-16 15:56:58 -0300

    refactor: split rules of the systemMessage

--- src/AIMessage.js
+++ src/AIMessage.js
@@ -5,15 +5,17 @@
  */
 
 class AIMessage {
-  #systemMessage = `
-  You will receive a piece of code, identify the programming language and correct the code following these instructions:
-  1. If you find a potential error, add a comment to the line above the potential error, with the following syntax: //TODO: <message>.
-  2. You should strictly follow the principles of clean code.
-  3. Return only the corrected code.
-  4. If you cannot identify the programming language, return the message "NO_FIXES".
-  5. If there are no corrections to be made, return the message "NO_FIXES".
-  6. If the code is correct, return the message "NO_FIXES".
-    `;
+  #systemMessage = `You will receive a piece of code, identify the programming language and correct the code following these instructions:\n`;
+  #rules = [
+    `Fix normal mistakes of the programming language detected.`,
+    `If you find a potential error, add a comment to the line above the potential error, with the following syntax: //TODO: <message>.`,
+    `You must strictly follow the principles of clean code.`,
+    `Apply early returns.`,
+    `Return only the corrected code.`,
+    `If you cannot identify the programming language, return the message "NO_FIXES".`,
+    `If there are no corrections to be made, return the message "NO_FIXES".`,
+    `If the code is correct, return the message "NO_FIXES".`,
+  ];
   #userMessage;
 
   /**
@@ -33,13 +35,19 @@ class AIMessage {
     return [
       {
         role: "system",
-        content: this.#systemMessage,
+        content: this.#getSystemMessage(),
       },
       {
         role: "user",
         content: this.#userMessage,
       },
     ];
+  }
+
+  #getSystemMessage() {
+    return this.#systemMessage.concat(
+      this.#rules.map((rule, index) => `${index + 1}. ${rule}`).join("\n")
+    );
   }
 }
 
--- tests/AIMessage.test.js
+++ tests/AIMessage.test.js
@@ -4,9 +4,23 @@ describe("AIMessage tests", () => {
   it("Quando chamar AIMessage.messages deve retornar um array de mensagens", () => {
     const message = new AIMessage("Mensagem do usuario");
     const messages = message.messages;
-    expect(messages[1]).toEqual({
-      role: "user",
-      content: "Mensagem do usuario",
-    });
+    expect(messages).toEqual([
+      {
+        role: "system",
+        content: `You will receive a piece of code, identify the programming language and correct the code following these instructions:
+1. Fix normal mistakes of the programming language detected.
+2. If you find a potential error, add a comment to the line above the potential error, with the following syntax: //TODO: <message>.
+3. You must strictly follow the principles of clean code.
+4. Apply early returns.
+5. Return only the corrected code.
+6. If you cannot identify the programming language, return the message "NO_FIXES".
+7. If there are no corrections to be made, return the message "NO_FIXES".
+8. If the code is correct, return the message "NO_FIXES".`,
+      },
+      {
+        role: "user",
+        content: "Mensagem do usuario",
+      },
+    ]);
   });
 });
