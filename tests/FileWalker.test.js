80f4d8762443d6a18e7828911d3ab3b9a57d222b
Pablo Winter <pablowinck123@gmail.com>
2023-07-16 13:56:08 -0300

    refactor: muda nome do arquivo e documenta metodos

---
+++ tests/FileWalker.test.js
@@ -0,0 +1,11 @@
+const FileWalker = require("../src/application/FileWalker");
+
+describe("Walker tests", () => {
+  it("Quando chamar walker passando uma pasta, para cada arquivo deve chamar um callback", async () => {
+    const callback = jest.fn();
+    const quantityOfFiles = 56;
+    const walker = new FileWalker("./tests/testFolder/src", callback);
+    await walker.walk();
+    expect(callback).toHaveBeenCalledTimes(quantityOfFiles);
+  });
+});
