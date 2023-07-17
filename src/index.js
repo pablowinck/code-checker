57fc6a6613ca80eca47666b65b2f6972201959f5
pablowinck <pablowinck123@gmail.com>
2023-07-17 15:53:20 -0300

    ia-fix: index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const { Configuration, OpenAIApi } = require("openai");
 const FileWalker = require("./FileWalker.js");
 const AIMessage = require("./AIMessage.js");
+
 const sourcePath = "./src";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,19 +12,24 @@ const openai = new OpenAIApi(configuration);
 const MAX_LENGTH = 2048;
 const RATE_LIMIT_SECONDS = 5;
 let counter = 0;
+
 const fileWalker = new FileWalker(sourcePath, async (path) => {
   counter++;
   const fileContent = fs.readFileSync(path, "utf8");
+
   if (fileContent.length > MAX_LENGTH) {
     console.log(`File ${path} is too long`);
     return;
   }
+
   await new Promise((resolve) =>
     setTimeout(resolve, RATE_LIMIT_SECONDS * 1000 * counter),
   );
+
   try {
     console.time(`File ${path} processed`);
     console.log(`Processing file ${path}...`);
+
     const { messages } = new AIMessage(fileContent);
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo-16k",
@@ -34,22 +40,27 @@ const fileWalker = new FileWalker(sourcePath, async (path) => {
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
     });
+
     fs.writeFileSync(
       `last_output_${new Date().getTime()}.json`,
       JSON.stringify(response.data.choices),
       "utf8",
     );
+
     const { content } = response.data.choices[0].message;
-    if (`${content}`.toUpperCase().includes("NO_FIXES")) {
+
+    if (`${content}`.toUpperCase() === "NO_FIXES") {
       console.log(`No fixes for file ${path}`);
       return;
     }
+
     console.log(`Writing file ${path}...`, content);
+
     fs.writeFileSync(path, content, "utf8");
     console.log(`File ${path} written`);
     console.timeEnd(`File ${path} processed`);
   } catch (error) {
-    console.error(`[${path}] -- Erro ao processar o arquivo\n`, error);
+    console.error(`[${path}] -- Error processing the file\n`, error);
   }
 });
 
